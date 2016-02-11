a137f5662c36d799f16bf35f45b0aaa2484aede7
jaeman1.park <jaeman1.park@samsung.com>
2016-02-11 19:07:41 +0900

    Implement audio tag

--- Makefile
+++ Makefile
@@ -86,9 +86,9 @@ else ifeq ($(HOST), tizen_wearable_arm)
   OUTDIR=out/tizen_$(ARCH)/$(TYPE)/$(MODE)
   	TIZEN_INCLUDE = dlog elementary-1 elocation-1 efl-1 ecore-x-1 eina-1 eina-1/eina eet-1 evas-1 ecore-1 ecore-evas-1 ecore-file-1 \
 			ecore-input-1 edje-1 eo-1 emotion-1 ecore-imf-1 ecore-con-1 eio-1 eldbus-1 efl-extension \
-			efreet-1 ecore-input-evas-1 ecore-audio-1 embryo-1 ecore-imf-evas-1 ethumb-1 eeze-1 eeze-1 e_dbus-1 e_dbus-1 dbus-1.0 freetype2
+			efreet-1 ecore-input-evas-1 ecore-audio-1 embryo-1 ecore-imf-evas-1 ethumb-1 eeze-1 eeze-1 e_dbus-1 e_dbus-1 dbus-1.0 freetype2 media
 
-	TIZEN_LIB = ecore evas rt efl-extension freetype
+	TIZEN_LIB = ecore evas rt efl-extension freetype capi-media-player
 
 	DEPENDENCY_INCLUDE =
 
--- src/dom/HTMLAudioElement.h
+++ src/dom/HTMLAudioElement.h
@@ -2,6 +2,9 @@
 #define __StarFishHTMLAudioElement__
 
 #include "dom/HTMLElement.h"
+#ifdef STARFISH_TIZEN_WEARABLE
+#include <player.h>
+#endif
 
 namespace StarFish {
 
@@ -12,6 +15,31 @@ public:
     {
         initScriptWrappable(this);
         m_isPlaying = false;
+
+#ifdef STARFISH_TIZEN_WEARABLE
+        player_create(&m_player);
+        if (player_create(&m_player) != PLAYER_ERROR_NONE) {
+            STARFISH_LOG_INFO("JMP, Can not create player handler.");
+        }
+        if (player_set_completed_cb(m_player, _on_player_completed_cb, nullptr) != PLAYER_ERROR_NONE) {
+            STARFISH_LOG_INFO("JMP, failed to register player state changed cb");
+        }
+
+        player_state_e player_state;
+        if (player_get_state(m_player, &player_state) != PLAYER_ERROR_NONE) {
+            STARFISH_LOG_INFO("JMP, failed to get player state");
+        }
+
+        if (player_state != PLAYER_STATE_IDLE) {
+            if (player_unprepare(m_player) != PLAYER_ERROR_NONE) {
+                STARFISH_LOG_INFO("JMP, unprepare error");
+            }
+        }
+#endif
+    }
+
+    static void _on_player_completed_cb(void *user_data) {
+        STARFISH_LOG_INFO("JMP, HTMLAudioElement::_on_player_completed_cb, start\n");
     }
 
     /* 4.4 Interface Node */
@@ -29,19 +57,62 @@ public:
     }
 
     void play() {
-        printf("Audio, play\n");
+        STARFISH_LOG_INFO("JMP, HTMLAudioElement::play, start\n");
+#ifdef STARFISH_TIZEN_WEARABLE
+        player_state_e state;
+        player_get_state(m_player, &state);
+
+        if(state != PLAYER_STATE_PLAYING)
+        {
+            STARFISH_LOG_INFO("JMP, HTMLAudioElement::play, player_start\n");
+            player_start(m_player);
+        }
+#endif
         m_isPlaying = true;
     }
 
     void pause() {
-        printf("Audio, pause\n");
+        STARFISH_LOG_INFO("JMP, HTMLAudioElement::pause, start\n");
+#ifdef STARFISH_TIZEN_WEARABLE
+        if (player_pause(m_player) != PLAYER_ERROR_NONE) {
+            STARFISH_LOG_INFO("JMP, HTMLAudioElement::pause, error\n");
+        }
+#endif
         m_isPlaying = false;
     }
 
     bool paused() { return !m_isPlaying; }
 
+    virtual void didAttributeChanged(QualifiedName name, String* old, String* value)
+    {
+        HTMLElement::didAttributeChanged(name, old, value);
+        if (name == document()->window()->starFish()->staticStrings()->m_src) {
+            m_src = document()->window()->starFish()->makeResourcePath(value);
+            setNeedsStyleRecalc();
+
+#ifdef STARFISH_TIZEN_WEARABLE
+            const char* path = m_src->utf8Data();
+            STARFISH_LOG_INFO("JMP, path = %s", path);
+            if (player_set_uri(m_player, path) != PLAYER_ERROR_NONE) {
+                STARFISH_LOG_INFO("JMP, set uri error. path = %s", path);
+            }
+
+            STARFISH_LOG_INFO("JMP, m_player = %p\n", m_player);
+            int prepare_error = player_prepare(m_player);
+            STARFISH_LOG_INFO("JMP, prepare_error = %d\n", prepare_error);
+            if (prepare_error != PLAYER_ERROR_NONE) {
+                STARFISH_LOG_INFO("JMP, prepare error");
+            }
+#endif
+        }
+    }
+
 protected:
     bool m_isPlaying;
+    String* m_src;
+#ifdef STARFISH_TIZEN_WEARABLE
+    player_h m_player;
+#endif
 };
 
 }
