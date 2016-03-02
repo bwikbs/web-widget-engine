911204e6f6c344a94ae8867d353e10d3032a35e5
seonghyun kim <sh8281.kim@samsung.com>
2016-03-02 18:00:53 +0900

    add missing file
    Signed-off-by: seonghyun kim <sh8281.kim@samsung.com>

---
+++ src/layout/StackingContext.cpp
@@ -0,0 +1,23 @@
+#include "StarFishConfig.h"
+#include "StackingContext.h"
+
+#include "FrameBox.h"
+
+namespace StarFish {
+
+StackingContext::StackingContext(FrameBox* owner, StackingContext* parent)
+{
+    m_owner = owner;
+    m_parent = parent;
+
+    if (m_parent) {
+        int32_t num = owner->style()->zIndex();
+        auto iter = m_parent->m_childContexts.find(num);
+        if (iter == m_parent->m_childContexts.end()) {
+            iter = m_parent->m_childContexts.insert(std::make_pair(num, new StackingContextChild())).first;
+        }
+        iter->second->push_back(this);
+    }
+}
+
+}
