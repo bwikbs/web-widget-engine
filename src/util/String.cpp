3720ee1b674a04bf2401a7cd768e2c442680828c
Ryan Choi <ryan.choi@samsung.com>
2016-03-29 18:10:57 +0900

    Pass getElementsByClassName-01.htm * Add test case

--- src/util/String.cpp
+++ src/util/String.cpp
@@ -223,7 +223,7 @@ String* String::replaceAll(const std::string& from, const std::string& to)
     if (m_isASCIIString) {
         std::string str = std::string(utf8Data());
         size_t start_pos = 0;
-        while((start_pos = str.find(from, start_pos)) != std::string::npos) {
+        while ((start_pos = str.find(from, start_pos)) != std::string::npos) {
             str.replace(start_pos, from.length(), to);
             start_pos += to.length(); // Handles case where 'to' is a substring of 'from'
         }
