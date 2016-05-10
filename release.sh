7e50849e93f6ddd86f09df9145ed5e65615d9d93
Eunji Jeong <eun-ji.jeong@samsung.com>
2016-05-10 14:45:39 +0900

    Add deviceapi into repository and update release.sh to build deviceapi

--- release.sh
+++ release.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+git submodule init
+git submodule update
 ./build_third_party.sh
 make clean
-make tizen_wearable_arm.lib.release -j
+make tizen_wearable_arm.lib.release TIZEN_DEVICE_API=true -j
 cp libWebWidgetEngine.so public/source/lib/tizen-wearable-2.3-target-arm/
 cp libWebWidgetEngine.so public/starting_with_ide/tizen-wearable-2.3-target-arm/lib/
-make tizen_wearable_emulator.lib.release -j
+make tizen_wearable_emulator.lib.release TIZEN_DEVICE_API=true -j
 cp libWebWidgetEngine.so public/source/lib/tizen-wearable-2.3-emulator-x86/
 cp libWebWidgetEngine.so public/starting_with_ide/tizen-wearable-2.3-emulator-x86/lib/
 cp -r inc/* public/source/include/
