f1675e1d4f494dd840bbdd55ea935bc3f279199e
Ryan Choi <ryan.choi@samsung.com>
2016-05-27 17:03:10 +0900

    Apply a quick fix for removing an extra tab in html.csv

--- tool/coverage/combineCsv.sh
+++ tool/coverage/combineCsv.sh
@@ -2,6 +2,11 @@
 
 mkdir -p out
 
+# FIX an extra tab in html.csv
+head -1 html.csv | sed 's/\t$//' > out/t.txt
+tail -n +2 html.csv > out/t1.txt
+cat out/t.txt out/t1.txt > html.csv
+
 run() {
     printf "\t\t$1" > out/label.txt
     c=`head -1 $2 | tr '\t' '\n' | wc -l`
@@ -18,11 +23,6 @@ run "CSS" css.csv cssLabel.txt
 
 cut -f3- out/htmlLabel.txt >  out/htmlLabel1.txt
 cut -f3- out/cssLabel.txt > out/cssLabel1.txt
-
-# FIX htmlLabel1.txt extra tab
-head -1 out/htmlLabel1.txt | sed 's/\t$//' > out/t.txt
-tail -n +2 out/htmlLabel1.txt > out/t1.txt
-cat out/t.txt out/t1.txt > out/htmlLabel1.txt
 
 paste out/domLabel.txt out/htmlLabel1.txt > out/t.txt
 paste out/t.txt out/cssLabel1.txt > coverage.csv
