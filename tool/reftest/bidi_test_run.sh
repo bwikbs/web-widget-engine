a3f76ccfe007d0bbfa039043a0b659f67f927895
Ryan Choi <ryan.choi@samsung.com>
2016-06-16 08:21:10 +0900

    Update bidi test script

--- tool/reftest/bidi_test_run.sh
+++ tool/reftest/bidi_test_run.sh
@@ -8,16 +8,13 @@ OUT=out/bidi
 mkdir -p $OUT
 
 for f in `ls $BIDI_PATH/*.html`; do
-    # ./StarFish $f --screen-shot=$f.tmp.png --width=900 --height=900 > /dev/null 2>&1
     out=$f
     out=`echo $out | sed 's/html-css/html-css\/x64/'`
     out=`echo $out | sed 's/\.html/-expected\.png/'`
     out=`basename $out`
     echo "$f -> $OUT/$out"
     #ELM_ENGINE="shot:file=$OUT/$out" ./StarFish $f --width=900 --height=900 > /dev/null 2>&1
-    #ELM_ENGINE="shot:file=$OUT/$out" ./StarFish $f --width=900 --height=900 > /dev/null 2>&1
     ./StarFish $f --screen-shot=$OUT/$out --width=900 --height=900 --regression-test > /dev/null 2>&1
     #r=`./imgdiffEvas.exe $f-expected.png $f.tmp.png`
     #rm -f $f.tmp.png
 done
-
