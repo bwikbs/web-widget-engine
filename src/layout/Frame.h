0d08cfa6d70182de6dd73190b00f8acb071e7169
MuHong Byun <mh.byun@samsung.com>
2016-07-04 15:30:10 +0900

    Revert "All positioned element make stacking context (It's incomplete solution)"
    This reverts commit c2c9b335314b5642ad5862853d6e05fdfede3e13.

--- src/layout/Frame.h
+++ src/layout/Frame.h
@@ -365,8 +365,7 @@ public:
             // https://www.w3.org/TR/CSS2/zindex.html
             // Appendix E. Elaborate description of Stacking Contexts
             // All positioned descendants with 'z-index: auto' or 'z-index: 0', in tree order. For those with 'z-index: auto', treat the element as if it created a new stacking context,
-            // m_flags.m_isEstablishesStackingContext = m_flags.m_isEstablishesStackingContext || (m_flags.m_isPositionedElement && style->IsSpecifiedZIndex());
-            m_flags.m_isEstablishesStackingContext = m_flags.m_isEstablishesStackingContext || (m_flags.m_isPositionedElement); // FIXME: All positioned descendants create a new stacking context. This could be temporary soluation.
+            m_flags.m_isEstablishesStackingContext = m_flags.m_isEstablishesStackingContext || (m_flags.m_isPositionedElement && style->IsSpecifiedZIndex());
             m_flags.m_isEstablishesStackingContext = m_flags.m_isEstablishesStackingContext || (style->opacity() != 1);
             m_flags.m_isEstablishesStackingContext = m_flags.m_isEstablishesStackingContext || (style->hasTransforms(this));
 
